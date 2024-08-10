b381c6e264554ddd16d4ea43a3fa080c525741fb
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-08-10 13:07:11 +0700

    feat[homepage]: Add homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main>
-      <div>Hello world!</div>
-    </main>
+    <div className="pt-6 flex gap-6">
+      <div className="hidden xl:block w-[20%]">Left Menu</div>
+      <div className="w-full lg:w-[70%] xl:w-[50%]">Center</div>
+      <div className="hidden lg:block w-[30%]">Right Menu</div>
+    </div>
   );
 }
