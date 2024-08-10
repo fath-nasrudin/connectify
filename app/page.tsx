26ff1358bc849e65752faddee8fea2c82a316e15
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-08-10 14:31:04 +0700

    Add Stories Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
+import Stories from '@/components/Stories';
+
 export default function Home() {
   return (
     <div className="pt-6 flex gap-6">
       <div className="hidden xl:block w-[20%]">Left Content</div>
-      <div className="w-full lg:w-[70%] xl:w-[50%]">Center Conent</div>
+      <div className="w-full lg:w-[70%] xl:w-[50%]">
+        <div className="flex flex-col gap-6">
+          <Stories />
+          <div>Add post</div>
+          <div>Feed</div>
+        </div>
+      </div>
       <div className="hidden lg:block w-[30%]">Right Content</div>
     </div>
   );
