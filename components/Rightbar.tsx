c0dc085369c695e0aee7475e5e1ffe795bf1b0bc
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-08-22 15:12:08 +0700

    feat[UserMediaCard]: Add UserMediaCard component

--- components/Rightbar.tsx
+++ components/Rightbar.tsx
@@ -3,11 +3,17 @@ import FriendRequests from './FriendRequests';
 import Birthdays from './Birthdays';
 import Ad from './Ad';
 import UserInfoCard from './UserInfoCard';
+import UserMediaCard from './UserMediaCard';
 
 function Rightbar({ userId }: { userId?: string }) {
   return (
     <div className="flex flex-col gap-6">
-      {userId ? <UserInfoCard userId={userId} /> : null}
+      {userId ? (
+        <>
+          <UserInfoCard userId={userId} />
+          <UserMediaCard userId={userId} />
+        </>
+      ) : null}
       <FriendRequests />
       <Birthdays />
       <Ad size="md" />
---
+++ components/UserMediaCard.tsx
@@ -0,0 +1,94 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+function UserMediaCard({ userId }: { userId: string }) {
+  return (
+    <div className="p-4 rounded-lg bg-white shadow-md ">
+      {/* Header */}
+      <div className="flex items-center justify-between">
+        <span className="text-sm text-gray-500">User Media</span>
+        <Link href="#">
+          <span className="text-blue-500 font-semibold text-xs">See all</span>
+        </Link>
+      </div>
+
+      {/* Content */}
+      <div className="flex gap-2 flex-wrap mt-4">
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/1108117/pexels-photo-1108117.jpeg?auto=compress&cs=tinysrgb&w=400"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/1108117/pexels-photo-1108117.jpeg?auto=compress&cs=tinysrgb&w=400"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/1108117/pexels-photo-1108117.jpeg?auto=compress&cs=tinysrgb&w=400"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/1108117/pexels-photo-1108117.jpeg?auto=compress&cs=tinysrgb&w=400"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/1108117/pexels-photo-1108117.jpeg?auto=compress&cs=tinysrgb&w=400"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/1108117/pexels-photo-1108117.jpeg?auto=compress&cs=tinysrgb&w=400"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/1108117/pexels-photo-1108117.jpeg?auto=compress&cs=tinysrgb&w=400"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/1108117/pexels-photo-1108117.jpeg?auto=compress&cs=tinysrgb&w=400"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserMediaCard;
