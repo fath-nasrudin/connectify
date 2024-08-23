bbd5f7e8ca5006309887c5ef68c53e4802a3eb28
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-08-23 10:47:13 +0700

    feat[ProfileCard]: Add ProfileCard component

---
+++ components/leftbar/ProfileCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ProfileCard() {
+  return (
+    <div className="p-4 rounded-lg bg-white shadow-md text-sm">ProfileCard</div>
+  );
+}
+
+export default ProfileCard;
