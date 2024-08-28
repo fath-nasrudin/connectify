8e94ee1d295c635f762a64d9d9c0c2cf093517af
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-08-28 20:09:55 +0700

    Change  userId component prop to user

--- components/Rightbar.tsx
+++ components/Rightbar.tsx
@@ -4,14 +4,15 @@ import Birthdays from './Birthdays';
 import Ad from './Ad';
 import UserInfoCard from './UserInfoCard';
 import UserMediaCard from './UserMediaCard';
+import { User } from '@prisma/client';
 
-function Rightbar({ userId }: { userId?: string }) {
+function Rightbar({ user }: { user?: User }) {
   return (
     <div className="flex flex-col gap-6">
-      {userId ? (
+      {user ? (
         <>
-          <UserInfoCard userId={userId} />
-          <UserMediaCard userId={userId} />
+          <UserInfoCard user={user} />
+          <UserMediaCard user={user} />
         </>
       ) : null}
       <FriendRequests />
